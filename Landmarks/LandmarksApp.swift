a99b9fd0752fa3ea8b2e29105e12c436403120d9
Tehillahk <tehillahk@outlook.com>
2024-09-02 21:17:18 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Tehillah Kangamba on 2024-09-02.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
