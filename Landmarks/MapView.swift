6ea16f9416817f38ea8e7ea70a0facda18330819
Tehillahk <kangambt@myumanitoba.ca>
2024-09-08 22:59:54 -0500

    Implemented map view

---
+++ Landmarks/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by Tehillah Kangamba on 2024-09-08.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
